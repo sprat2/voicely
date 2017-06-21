0877ab8ca44faaaca67f4c190e76ca2754c7729d
Server <NathanRollins@Live.com>
2017-06-21 17:11:20 -0600

    fixed child theme's style.css precedence

--- voicely/public_html/wp-content/themes/Avada-child/functions.php
+++ voicely/public_html/wp-content/themes/Avada-child/functions.php
@@ -1,8 +1,13 @@
 <?php
 
-// Import Avada's style sheet
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+// Import Avada's style sheet before this child theme's style sheet
+function my_theme_enqueue_styles() {
+    $parent_style = 'parent-style'; // More efficient (yet more error-prone) if replaced per https://codex.wordpress.org/Child_Themes
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
 }
-
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
