d2e3d1b90f9014a55c463d7b7e5e4233b894eabf
Server <NathanRollins@Live.com>
2017-06-21 19:03:54 -0600

    Halting current theme experimentation/exploration to pursue things specified in the email from Spencer

--- voicely/public_html/wp-content/themes/Avada-child/functions.php
+++ voicely/public_html/wp-content/themes/Avada-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-// Import Avada's style sheet before this child theme's style sheet
+// Import Avada's style sheet before this child theme's style sheet (and cache-bust)
 function my_theme_enqueue_styles() {
     $parent_style = 'parent-style'; // More efficient (yet more error-prone) if replaced per https://codex.wordpress.org/Child_Themes
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
